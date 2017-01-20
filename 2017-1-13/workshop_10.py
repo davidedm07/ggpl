50afd53556b560f32adcda3fd7db981648f19c12
davidedm07 <davide.dimaria07@gmail.com>
2017-01-20 16:14:21 +0100

    workshop_10 not completed yet

---
+++ 2017-1-13/workshop_10.py
@@ -0,0 +1,11 @@
+from pyplasm import *
+import csv
+from res import workshop_08 as w08
+from res import workshop_03 as w03
+def ggpl_multistory_house():
+	house = w08.ggpl_house_builder()
+	VIEW(house)
+	
+
+if __name__ == "__main__":
+	ggpl_multistory_house()
